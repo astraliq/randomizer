3aafbae0ec9fbfa26d0bece4d1bd7ae2c12f61a2
astraliq <astraliq457@gmail.com>
2020-02-05 21:35:55 +0300

    some fix

--- www/js/main.js
+++ www/js/main.js
@@ -355,6 +355,11 @@ class Films {
                     </div>
                 </div>
             </div>
+			<div class="other-cat">
+				Кроме фильмов наш генератор выдаёт варианты из
+				<a href="#" class="link-in-text">других категорий</a>,
+				например, &laquo;<a href="#" class="link-in-text" onclick="quote.init()">Цитата</a>&raquo;
+			</div>
 		`);
     }
 };
